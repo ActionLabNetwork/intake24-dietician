318ff8aff48aaf25499fb848868153515fd2e8b6
Christopher <christopherowenprawira@gmail.com>
2023-09-13 16:32:05 +1000

    feat(tsoa): add generic error handler and missing route handler

--- apps/auth/src/app.ts
+++ apps/auth/src/app.ts
@@ -1,8 +1,13 @@
-import express, { Response as ExResponse, Request as ExRequest } from 'express'
+import express, {
+  Response as ExResponse,
+  Request as ExRequest,
+  NextFunction,
+} from 'express'
 import bodyParser from 'body-parser'
 import { RegisterRoutes } from '../build/routes'
 import swaggerUi from 'swagger-ui-express'
 import cors from 'cors'
+import { ValidateError } from 'tsoa'
 
 export const app = express()
 app.use(bodyParser.json())
@@ -12,3 +17,37 @@ app.use('/docs', swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
 app.use(cors())
 
 RegisterRoutes(app)
+
+// TSOA Catch-all missing route handler
+app.use((_req, res: ExResponse) => {
+  res.status(404).send({
+    message: 'Not Found',
+  })
+})
+
+// TSOA Error Handler
+app.use(
+  (
+    err: unknown,
+    req: ExRequest,
+    res: ExResponse,
+    next: NextFunction,
+    // eslint-disable-next-line max-params
+  ): ExResponse | undefined => {
+    if (err instanceof ValidateError) {
+      console.warn(`Caught Validation Error for ${req.path}:`, err.fields)
+      return res.status(422).json({
+        message: 'Validation Failed',
+        details: err?.fields,
+      })
+    }
+    if (err instanceof Error) {
+      return res.status(500).json({
+        message: 'Internal Server Error',
+      })
+    }
+
+    next()
+    return
+  },
+)
