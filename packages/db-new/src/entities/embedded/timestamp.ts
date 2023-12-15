b5e62718cbaf64ff556b04854bd48c6a2ac0d8c9
Kent Hou Man <knthmn0@gmail.com>
2023-12-15 11:08:13 +1100

    refactor: [wip] remove old files

--- packages/db-new/src/entities/embedded/timestamp.ts
+++
@@ -1,9 +0,0 @@
-import { CreateDateColumn, UpdateDateColumn } from 'typeorm'
-
-export class Timestamp {
-  @CreateDateColumn()
-  public declare createdAt: Date
-
-  @UpdateDateColumn()
-  public declare updatedAt: Date
-}
