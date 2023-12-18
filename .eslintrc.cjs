3f33f84804193bb06bf70c0b64da815c617c71ab
Kent Hou Man <knthmn0@gmail.com>
2023-12-18 11:32:17 +1100

    chore: make ESLint automatically fix unused imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['alloy', 'alloy/typescript'],
   ignorePatterns: ['**/*.js'],
+  plugins: ["unused-imports"],
   env: {
     browser: true,
     node: true,
@@ -14,6 +15,22 @@ module.exports = {
     // myGlobal: false
   },
   rules: {
-    "max-params": "off"
+    "max-params": "off",
+    // auto fix for unused imports
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        // allow _, _0, _1, etc. for skipping unused arguments
+        varsIgnorePattern: "^_\\d*$",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    // disable red squiggles
+    "prefer-const": "warn",
+    "no-empty": "warn",
   },
 }
