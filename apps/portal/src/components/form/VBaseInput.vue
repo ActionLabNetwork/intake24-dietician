3abb8311c6dc4b97fc8b721d7d3b09250df5d206
Christopher <christopherowenprawira@gmail.com>
2024-01-29 14:34:35 +1100

    refactor(portal): try out vee-validate on login form

---
+++ apps/portal/src/components/form/VBaseInput.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="d-flex flex-column">
+    <div :class="['form-label pb-2', labelClass]">
+      <slot />
+    </div>
+    <div v-if="type === 'tel'" class="">
+      <v-phone-input
+        :model-value="value?.toString()"
+        label=""
+        country-label=""
+        country-icon-mode="svg"
+        density="compact"
+        :invalid-message="`Invalid mobile number for ${country}`"
+        guess-country
+        default-country="AU"
+        :phone-props="{
+          flat: true,
+          variant: 'solo-filled',
+          density: 'comfortable',
+        }"
+        :country-props="{
+          flat: true,
+          variant: 'solo-filled',
+          density: 'comfortable',
+        }"
+        @update:model-value="(value: string) => emit('update', value)"
+        @update:country="country = $event"
+      />
+    </div>
+    <div
+      v-else
+      :style="bordered ? 'background: white' : ''"
+      :class="{
+        'pa-3 my-3 d-flex rounded-lg elevation-1': bordered,
+      }"
+    >
+      <v-text-field
+        v-model="fieldValue"
+        flat
+        :required="required ?? false"
+        :type="type ?? 'text'"
+        :placeholder="placeholder ?? ''"
+        :error-messages="errorMessage"
+        :autocomplete="autocomplete ?? 'off'"
+        variant="solo-filled"
+        density="comfortable"
+        :append-inner-icon="suffixIcon"
+        :append-icon="suffixIconOuter"
+        :name="name"
+        :rules="rules ?? []"
+        :readonly="readonly ?? false"
+        :suffix="suffix"
+        :bordered="bordered"
+        :data-cy="dataCy"
+        @click:append-inner="handleIconClick"
+        @click:append="handleOuterIconClick"
+      >
+      </v-text-field>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { VPhoneInput } from 'v-phone-input'
+import 'v-phone-input/dist/v-phone-input.css'
+import 'flag-icons/css/flag-icons.min.css'
+import { ref } from 'vue'
+import { useField } from 'vee-validate'
+
+const props = defineProps<{
+  name: string
+  type?: HTMLInputElement['type']
+  placeholder?: string
+  autocomplete?: HTMLInputElement['autocomplete']
+  suffix?: string
+  suffixIcon?: string
+  suffixIconOuter?: string
+  value?: string | number
+  rules?: ((value: string) => boolean | string)[]
+  readonly?: boolean
+  labelClass?: string
+  required?: boolean
+  bordered?: boolean
+  dataCy?: string
+  handleIconClick?: () => void
+  handleOuterIconClick?: () => void
+}>()
+const emit = defineEmits<{
+  update: [value: string]
+}>()
+const { value: fieldValue, errorMessage } = useField(() => props.name)
+
+const country = ref('AU')
+</script>
+
+<style scoped>
+.form-label {
+  font-size: 0.875rem;
+  font-weight: 500;
+}
+</style>
